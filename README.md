e51a43a9a53ad6b5ba9071e404f908b225a13fd3
celia103 <celiayych@gmail.com>
2024-01-16 08:30:12 +0000

    update markdown and fix enter N/A issues

--- README.md
+++
@@ -1,2 +0,0 @@
-# W11-Dynamic-README-Generator
-The "Dynamic README Generator" is a tool designed to simplify the creation of thorough and well-organized README files for software projects. 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,49 +1,72 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+	const sectionNames = Object.keys(data).filter(
+		(key) =>
+			key !== "title" &&
+			key !== "description" &&
+			key !== "name" &&
+			key !== "email" &&
+			key !== "GitHub" &&
+			key !== "usageImage" &&
+			key !== "collaboratorGitHub" &&
+			key !== "collaboratorEmail" &&
+			key !== "contributeText"
+	);
 
-  const sectionNames = Object.keys(data).filter((key) => key 
-  !== 'title' 
-  && key !== 'description' 
-  && key !== 'name' 
-  && key !== 'email' 
-  && key !== 'GitHub'
-  && key !== 'usageImage' 
-  && key !== 'collaboratorGitHub'
-  && key !== 'collaboratorEmail'
-  && key !== 'contributeText');
-  
-  const titleMap = {
-    'installation': 'Installation',
-    'usage':'Usage',
-    'credits':'Credits',
-    'license':'License',
-    'features':'Features',
-    'contribute':'How to Contribute',
-    'test':'Tests',
-  }
+	const titleMap = {
+		installation: "Installation",
+		usage: "Usage",
+		credits: "Credits",
+		license: "License",
+		features: "Features",
+		contribute: "How to Contribute",
+		test: "Tests",
+	};
 
-  const tableOfContents = sectionNames
-    .map((section) => {
-      // Check if the section has a non-N/A answer
-      if (data[section] && data[section] !== 'N/A') {
-        return `- [${titleMap[section]}](#${titleMap[section].toLowerCase().split(' ').join('-')})`;
-      }
-      return ''; // Return an empty string if N/A or no answer
-    })
-    .filter(section => section !== '') // Filter out empty strings
-    .join('\n');
+	const tableOfContents = sectionNames
+		.map((section) => {
+			// Check if the section has a non-N/A answer
+			if (data[section] && data[section] !== "N/A") {
+				return `- [${titleMap[section]}](#${titleMap[section]
+					.toLowerCase()
+					.split(" ")
+					.join("-")})`;
+			}
+			return ""; // Return an empty string if N/A or no answer
+		})
+		.filter((section) => section !== "") // Filter out empty strings
+		.join("\n");
 
-  const creditList = data.credits === 'N/A' ? '' :
-  data.credits
-  .map((credit, index) => `- [${data.credits[index]} GitHub](https://github.com/${(data.collaboratorGitHub[index] || '').toString().toLowerCase().split(' ').join('-')})`)
-  .join('\n');
-  
-  const contactList = data.credits === 'N/A' ? '' :
-  data.collaboratorEmail
-    .map((email, index) => `- [${data.credits[index]} Email](${email.toLowerCase().split(' ').join('-')})`)
-    .join('\n');
+	const creditList =
+		(data.credits.length === 0) || (data.credits[0] === "N/A")
+			? ""
+			: data.credits
+					.map(
+						(credit, index) =>
+							`- [${data.credits[index]} GitHub](https://github.com/${(
+								data.collaboratorGitHub[index] || ""
+							)
+								.toString()
+								.toLowerCase()
+								.split(" ")
+								.join("-")})`
+					)
+					.join("\n");
 
-  return `
+	const contactList =
+    (data.credits.length === 0) || (data.credits[0] === "N/A")
+			? ""
+			: data.collaboratorEmail
+					.map(
+						(email, index) =>
+							`- [${data.credits[index]} Email](${email
+								.toLowerCase()
+								.split(" ")
+								.join("-")})`
+					)
+					.join("\n");
+
+	return `
 # ${data.title}
 
 ${data.description && "## Description"}
@@ -61,8 +84,12 @@ ${data.usage}
 ![${data.usageImage}](assets/images/${data.usageImage})
 
 ## Credits
-${data.credits === 'N/A' ? '' : `- [${data.name} GitHub](https://github.com/${data.GitHub})`}
-${creditList}
+${
+	data.credits === "N/A"
+		? ""
+		: `- [${data.name} GitHub](https://github.com/${data.GitHub})`
+}
+${data.credits === "N/A" ? "" : creditList}
 
 ## License
 ${data.license}
@@ -78,8 +105,9 @@ ${data.test}
 
 ## Questions
 If you have any questions or need further clarification, feel free to open an issue or contact the maintainers directly:
-${data.credits === 'N/A' ? '' : `- [${data.name} Email](${data.email})`}
-${contactList}
+- [${data.name} Email](${data.email})
+${data.credits.length === 0 || data.credits[0] === "N/A" ? "" : contactList}
+
 
 `;
 }
