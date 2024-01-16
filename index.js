c86861ca9f4f28aa9da9b5e5c6c2b4f8bc6aec93
celia103 <celiayych@gmail.com>
2024-01-16 06:52:26 +0000

    create generator and upload node_modules

--- index.js
+++ index.js
@@ -5,17 +5,127 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+  {
+    type: "input",
+    name: "name",
+    message:"What is your name?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message:"What is your email?",
+  },
+  {
+    type: "input",
+    name: "GitHub",
+    message:"What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message:"Enter the title of your project:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message:"Provide a brief description of your project.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message:"What are the steps required to install your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message:"Provide instructions and examples for use.",
+  },
+  {
+    type: "input",
+    name: "usageImage",
+    message:"Put example screenshot in `assets/images` folder in your repository. Provide the file name ONLY.",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message:"List your collaborator(s), if any. (Separate with commas)",
+    filter: (input) => input.split(',').map((item) => item.trim()),
+  },
+  {
+    type: "input",
+    name: "collaboratorGitHub",
+    message:"What are their GitHub username?",
+    filter: (input) => input.split(',').map((item) => item.trim()),
+  },
+  {
+    type: "input",
+    name: "collaboratorEmail",
+    message:"What are their emails?",
+    filter: (input) => input.split(',').map((item) => item.trim()),
+  },
+  {
+    type: "list",
+    name: "license",
+    message:"Choose a license for your project. ",
+    choices: ['MIT License', 'Apache License 2.0',
+    'GNU General Public License v3.0',
+    'BSD 2-Clause "Simplified" License',
+    'BSD 3-Clause "New" or "Revised" License',
+    'Boost Software License 1.0',
+    'Creative Commons Zero v1.0 Universal',
+    'Eclipse Public License 2.0',
+    'GNU Affero General Public License',
+    'GNU Lesser General Public License',
+    'Mozilla Public License 2.0',
+    'The Unlicense']
+  },
+  {
+    type: "input",
+    name: "features",
+    message:"List features, if any.",
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message:"Provide guideline link for how to contribute it, if any.",
+  },
+  {
+    type: "input",
+    name: "contributeText",
+    message:"Provide guideline name",
+  },
+  {
+    type: "input",
+    name: "test",
+    message:"Provide instructions on running tests.",
+  },
+
 
 ];
 
-// function to write README file
+// function to write README file'
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Successfully generated!`);
+    }
+  });
 }
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then((data) => {
+    const fileName = `${data.title.toLowerCase().split(' ').join('')}-README.md`;
+    const markdownContent = generateMarkdown(data);
+    writeToFile(fileName, markdownContent);
+  });
 }
 
 // function call to initialize program
 init();
+
+
+
+//liscense = select box
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,81 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+
+  const sectionNames = Object.keys(data).filter((key) => key 
+  !== 'title' 
+  && key !== 'description' 
+  && key !== 'name' 
+  && key !== 'email' 
+  && key !== 'GitHub'
+  && key !== 'usageImage' 
+  && key !== 'collaboratorGitHub'
+  && key !== 'collaboratorEmail'
+  && key !== 'contributeText');
+  
+  const titleMap = {
+    'installation': 'Installation',
+    'usage':'Usage',
+    'credits':'Credits',
+    'license':'License',
+    'features':'Features',
+    'contribute':'How to Contribute',
+    'test':'Tests',
+  }
+
+  const tableOfContents = sectionNames
+    .map((section) => {
+      // Check if the section has a non-N/A answer
+      if (data[section] && data[section] !== 'N/A') {
+        return `- [${titleMap[section]}](#${titleMap[section].toLowerCase().split(' ').join('-')})`;
+      }
+      return ''; // Return an empty string if N/A or no answer
+    })
+    .filter(section => section !== '') // Filter out empty strings
+    .join('\n');
+
+  const creditList = data.credits === 'N/A' ? '' :
+  `- [${data.credits}](https://github.com/${(data.collaboratorGitHub || '').toString().toLowerCase().split(' ').join('-')})`;
+
+  const contactList = data.credits === 'N/A' ? '' :
+  `- [${data.credits} Email](${(data.collaboratorEmail || '').toString().toLowerCase().split(' ').join('-')})`;
+
+  return `
+# ${data.title}
+
+${data.description && "## Description"}
+${data.description}
+
+## Table of Contents
+${tableOfContents}
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+![${data.usageImage}](assets/images/${data.usageImage})
+
+## Credits
+${data.credits === 'N/A' ? '' : `- [${data.name} GitHub](https://github.com/${data.GitHub})`}
+${creditList}
+
+## License
+${data.license}
+
+## Features
+${data.features}
+
+## How to Contribute
+[${data.contributeText}](${data.contribute})
+
+## Tests
+${data.test}
+
+## Questions
+If you have any questions or need further clarification, feel free to open an issue or contact the maintainers directly:
+${data.credits === 'N/A' ? '' : `- [${data.name} Email](${data.email})`}
+${contactList}
 
 `;
 }
